1bf4a4d75c2f34a17b7f2aa6c67cf818a2a4f5d8
Jeremi F <jjfd428@gmail.com>
2021-02-10 16:51:47 -0500

    I made it a little cleaner with apogee_m and apogee_t variables. Also it has a noise adding function at the top, but beware the algorithm is too dumb to handle it yet.

--- simulator.py
+++ simulator.py
@@ -1,20 +1,30 @@
 import pandas as pd
 import time
+import random
 #import spreadsheet as ss
 # Event APOGEE occurred at t=8.8872 seconds
 
-class Flight_Computer():
-    def __init__(self, path_to_csv_of_simulation):
-        self.sim = pd.DataFrame(pd.read_csv(path_to_csv_of_simulation))
-        self.correct_apogee_t = 8.8872#seconds; change this as needed between different openrocket simulations
+def noisy_sim(sim):
+    noisy_sim = sim
+    for index, row in noisy_sim.iterrows():
+        if index <10:
+            row["Altitude (m)"] = row["Altitude (m)"] + random.uniform(0,1)
+        else:
+            row["Altitude (m)"] = row["Altitude (m)"] + random.uniform(-1,1)
+    return noisy_sim
 
+
+
+class Flight_Computer():
+    def __init__(self, path_to_csv_of_simulation, apogee_t, apogee_m):
+        self.sim = pd.DataFrame(pd.read_csv(path_to_csv_of_simulation))
+        self.correct_apogee_t = apogee_t#seconds; change this as needed between different openrocket simulations
+        self.correct_apogee_m =apogee_m 
 
 
     @staticmethod
     def instant_velocity(time_now, time_previous, altitude_now, altitude_previous):
         return (altitude_now-altitude_previous)/(time_now-time_previous)
-
-
     
 
     def check_liftoff(self, velocities):
@@ -28,7 +38,8 @@ class Flight_Computer():
             return True
         else:
             return False
-        
+
+    #apogee detector needs to be changed to a counter system because the true-false approach doesn't work with noise
     def closetozero(self, velocity):
         if -1 <= velocity <= 1:
             return True
@@ -51,32 +62,36 @@ class Flight_Computer():
         accuracy = abs(prediction-correct) / correct
         return round(1 - accuracy, 5)
     #hello
-    def Fly(self):
+    def Fly(self, simulation_frame):
         velocities = []
         apogee = False
         liftoff = False
-        for index, row in self.sim.iterrows():
+        for index, row in simulation_frame.iterrows():
             #print(index, row["Time (s)"], row["Altitude (m)"])
 
             if index == 0:
                 continue
             else:
                 #v is in m/s
-                v = self.instant_velocity(self.sim["Time (s)"][index],self.sim["Time (s)"][index-1], self.sim["Altitude (m)"][index],self. sim["Altitude (m)"][index-1])
+                v = self.instant_velocity(simulation_frame["Time (s)"][index],simulation_frame["Time (s)"][index-1], simulation_frame["Altitude (m)"][index],simulation_frame["Altitude (m)"][index-1])
                 velocities.append(v)
                 if index > 5:
                     if liftoff == False:
                         liftoff = self.check_liftoff(velocities)
-            
 
             if liftoff== True:
                 if index > 20:
                     apogee = self.apogeeDetector(velocities)
                     if apogee == True:
-                        print("APOGEE!!! ")
+                        #print("APOGEE!!! ")
+                        return [row["Time (s)"],row["Altitude (m)"]]
                         break
 
+apogee_m = 408.54
+apogee_t = 8.8872
+path_to_sim = "//Users//jeremi//Documents//Flight_computer//simulation1.csv"
+Eddie = Flight_Computer(path_to_sim, apogee_m,apogee_t)
+#print(noise_1[:10])
+#print(Eddie.sim[0:10])
 
-
-Eddie_the_Computer = Flight_Computer("//Users//jeremi//Documents//Flight_computer//simulation1.csv")
-Eddie_the_Computer.Fly()
+print(Eddie.Fly(Eddie.sim))
