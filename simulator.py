9292593c7175d3cf2d13ef2a6f560b1c5908d662
Jeremi F <jjfd428@gmail.com>
2021-02-12 11:53:28 -0500

    Did some cool data analytics stuff

--- simulator.py
+++ simulator.py
@@ -1,16 +1,18 @@
+#!/usr/local/bin/python3
 import pandas as pd
 import time
 import random
-#import spreadsheet as ss
+from progressbar import ProgressBar
+
 # Event APOGEE occurred at t=8.8872 seconds
 
 def noisy_sim(sim):
     noisy_sim = sim
     for index, row in noisy_sim.iterrows():
-        if index <10:
-            row["Altitude (m)"] = row["Altitude (m)"] + random.uniform(0,1)
+        if index <30:
+            row["Altitude (m)"] = row["Altitude (m)"] + random.uniform(0,0.5)
         else:
-            row["Altitude (m)"] = row["Altitude (m)"] + random.uniform(0,1)
+            row["Altitude (m)"] = row["Altitude (m)"] + random.uniform(-0.5,0.5)
     return noisy_sim
 
 
@@ -26,6 +28,15 @@ class Flight_Computer():
     def instant_velocity(time_now, time_previous, altitude_now, altitude_previous):
         return (altitude_now-altitude_previous)/(time_now-time_previous)
     
+    @staticmethod
+    def avg(lst):
+        return sum(lst)/len(lst)
+
+    def rolling_avg(self, velocities, block_size):
+        rolled_vs = []
+        for i in range(0,len(velocities)-1-block_size):
+            rolled_vs.append(self.avg(velocities[i:i+block_size]))
+        return rolled_vs
 
     def check_liftoff(self, velocities):
         count = 0
@@ -76,13 +87,14 @@ class Flight_Computer():
                 velocities.append(v)
                 if index > 5:
                     if liftoff == False:
-                        liftoff = self.check_liftoff(velocities)
+                        liftoff = self.check_liftoff(self.rolling_avg(velocities,6))
 
             if liftoff== True:
                 if index > 20:
                     apogee = self.apogeeDetector(velocities)
                     if apogee == True:
-                        return [row["Time (s)"],row["Altitude (m)"], self.calculate_accuracy(row["Altitude (m)"], self.correct_apogee_m)]
+                        #print(self.rolling_avg(velocities, 3))
+                        return [row["Time (s)"],row["Altitude (m)"], self.calculate_accuracy(row["Time (s)"], self.correct_apogee_t)]
                         break
 
 apogee_m = 408.54
@@ -97,19 +109,32 @@ path_to_sim = "//Users//jeremi//Documents//Flight_computer//simulation1.csv"
 #print("Simulation with noise: ", Eddie_for_noise.Fly(noise1))
 
 
-def super_tester():
+def gather_data(test_size):
+    
     Eddie_test = Flight_Computer(path_to_sim, apogee_t,apogee_m)
     accuracies = []
-    for i in range(20):
+    altitudes = []
+    total_fails = 0
+    pbar = ProgressBar()
+    for i in pbar(range(test_size)):
         try:
             noised = noisy_sim(Eddie_test.sim)
             results = Eddie_test.Fly(noised)
             accuracies.append(results[2])
+            altitudes.append(results[1])
         except Exception:
-            accuracies.append(0)
+            total_fails += 1
             continue
-    return accuracies
-accuracies = super_tester()
-avg = lambda lst: sum(lst)/len(lst)
+
+    return accuracies, total_fails, test_size, altitudes
+'''
+results = super_tester(20)
+accuracies = results[0]
+failrate = results[1] / results[2]
+altitudes = results[3]
+avg = lambda lst: round(sum(lst)/len(lst),4)
 print("Average accuracy: ",avg(accuracies))
-print("Worst performance: ", min(accuracies))
+print("Worst accuracy in set: ", min(accuracies))
+print("Percentage total failure: ", failrate)
+print("Average altitude: ", avg(altitudes))
+'''
